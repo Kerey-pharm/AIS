99e09167bcbceb0059c454dfdbb883b2f9d4c7c6
Dauren Mussa <dauren.mussa@gmail.com>
2014-05-23 19:47:12 +0300

    working GoodClass with EJB and WebService

---
+++ services/src/main/java/kz/kerey/constants/Constants.java
@@ -0,0 +1,14 @@
+package kz.kerey.constants;
+
+public class Constants {
+
+	public static Integer exceptionIsOccured = -1;
+	
+	public static Integer isOk = 0;
+	public static Integer objectIsNull = 1;
+	public static Integer objectIsEmpty = 2;
+	public static Integer objectExists = 3;
+	
+	
+	
+}
---
+++ services/src/main/java/kz/kerey/logic/services/types/GoodRequest.java
@@ -0,0 +1,17 @@
+package kz.kerey.logic.services.types;
+
+public class GoodRequest {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
